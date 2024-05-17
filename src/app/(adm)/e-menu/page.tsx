b1efe0d4d36a98fcb01df1e773324bcc0bae9961
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-17 11:12:38 -0300

    fix: order by payment

--- src/app/(adm)/e-menu/page.tsx
+++ src/app/(adm)/e-menu/page.tsx
@@ -6,6 +6,9 @@ export default async function Page() {
   const allGuestsMenus = (await getAllGuestMenu()) || [];
   const orderedGuestsMenus = orderAllGuestByDate(allGuestsMenus);
 
+  const nopayedGuestsMenus = orderedGuestsMenus.filter((guest) => !guest.isPaid);
+  const payedGuestsMenus = orderedGuestsMenus.filter((guest) => guest.isPaid);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center pt-10">
       <h1>Menu Editor</h1>
@@ -13,7 +16,10 @@ export default async function Page() {
       <br />
 
       <ul>
-        {orderedGuestsMenus.map((gm) => (
+        {nopayedGuestsMenus.map((gm) => (
+          <ButtonsEditMenu key={gm.id} guestMenu={gm} />
+        ))}
+        {payedGuestsMenus.map((gm) => (
           <ButtonsEditMenu key={gm.id} guestMenu={gm} />
         ))}
       </ul>
