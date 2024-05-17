c1be035758b721a7be1a1aba59307e8c5498bb51
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-17 20:54:52 -0300

    fix: more no tacc options soon

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
 
       <section className="max-w-md  p-5">
         <h2>Reserva tu Menú</h2>
-        <p className="text-xs text-info">* Próximamente opciones sin TACC</p>
+        <p className="text-xs text-info">* Próximamente más opciones sin TACC</p>
 
         <SelectMenu />
       </section>
