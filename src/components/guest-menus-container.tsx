5c11fb29d977253cc5052998a2dbd5ddec83929f
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-17 12:54:47 -0300

    feat: whatsapp msg

--- src/components/guest-menus-container.tsx
+++ src/components/guest-menus-container.tsx
@@ -3,6 +3,7 @@
 import { GuestMenu } from '../firebase/types';
 import { ButtonTACC } from './button-tacc';
 import { useMenuGuestState } from '../hooks/use-menu-guest-state';
+import Link from 'next/link';
 
 interface GuestMenusContainerProps {
   guestMenu: GuestMenu;
@@ -20,6 +21,10 @@ export function GuestMenusContainer({ guestMenu }: GuestMenusContainerProps) {
       'Pischiwan! te copié el alias para tranferir al portapapeles. \n \n Alias: fiesta.alfonso.ar \n \n Esa cuenta es única para la fiesta. \n \n Luego yo me fijo y te apruebo el pago.'
     );
   }
+
+  const phone = process.env.NEXT_PUBLIC_ADMIN_PHONE || '';
+
+  const text = `*Aviso de Pago de:* ${guest} %0A *Total:* $${totalPrice} %0A *Código:* ${id} %0A (Adjuntar comprobante de pago)`;
 
   return (
     <li className="collapse collapse-plus bg-base-200 my-3">
@@ -39,13 +44,25 @@ export function GuestMenusContainer({ guestMenu }: GuestMenusContainerProps) {
           ))}
         </ul>
         <p>
-          <span>Total:</span>$ {totalPrice} |{' '}
+          <span>Total:</span>$ {totalPrice}
           {isPaid ? (
-            'Pagado ✅'
+            ' | Pagado ✅'
           ) : (
-            <button onClick={onPaid} className="btn-md text-xs">
-              Pagar
-            </button>
+            <div className="flex justify-center gap-1">
+              <button onClick={onPaid} className="btn-md text-xs">
+                Pagar
+              </button>
+
+              <Link
+                className="btn-md text-xs p-1 px-2 flex flex-col"
+                href={`https://api.whatsapp.com/send?phone=${phone}&text=${text}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>Avisar por</span>
+                <span>WhatsApp</span>
+              </Link>
+            </div>
           )}
         </p>
       </div>
