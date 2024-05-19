4121b6f96e0d5a0d00ec6bd860a957288a4d12e5
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-19 02:52:33 -0300

    feat: footer alfonso app link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,28 @@
 import Image from 'next/image';
 import imgInstagram from '../../public/instagram.svg';
+import imgAlfonsoIcon from '../../public/alfonso-app-icon-512x512.png';
 import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full mb-5 px-5 text-xs flex justify-between z-[-9]">
+    <footer className="mb-5 px-5 text-xs flex justify-between z-[-9]">
       <div className="flex flex-col">
         <p>By Alfonso Montes de Oca</p>
         <small>© 2024</small>
       </div>
-      <Link
-        href={'https://www.instagram.com/montesdeoca_alfonso/'}
-        rel="noopener noreferrer"
-        className="border-none "
-      >
-        <Image src={imgInstagram} alt="Instagram" width={30} height={30} priority />
-      </Link>
+
+      <div className="flex">
+        <Link href={'https://www.alfonso.ar/'} rel="noopener noreferrer" className="border-none ">
+          <Image src={imgAlfonsoIcon} alt="Instagram" width={30} height={30} priority />
+        </Link>
+        <Link
+          href={'https://www.instagram.com/montesdeoca_alfonso/'}
+          rel="noopener noreferrer"
+          className="border-none "
+        >
+          <Image src={imgInstagram} alt="Instagram" width={30} height={30} priority />
+        </Link>
+      </div>
     </footer>
   );
 }
