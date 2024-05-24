c06e5817272ba61fccf87aee5aaaa2675f8b9336
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-24 10:01:08 -0300

    feat: identify organizers

--- src/app/(bg-video)/[vip]/SaveVipUrl.tsx
+++ src/app/(bg-video)/[vip]/SaveVipUrl.tsx
@@ -1,16 +1,28 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useVipUrlState } from 'hooks';
 import { useShowNavState } from 'hooks';
 import { GuestNav } from 'components/guest-nav';
+import { VIP } from 'firebase/types';
+import { getAllVip } from 'firebase';
 
 export function SaveVipUrl({ vipUrl }: { vipUrl: string }) {
-  const { setVipUrl } = useVipUrlState();
+  const { setVipUrl, setIsOrganizer } = useVipUrlState();
   const { isShowNav } = useShowNavState();
 
   useEffect(() => {
     setVipUrl(vipUrl);
+    getAllVip()
+      .then((vips) => {
+        const findVip = vips.find((vip) => vip.guest === decodeURIComponent(vipUrl));
+        if (findVip) {
+          const isOrganizer = findVip.rol.toLowerCase().includes('organizador');
+          setIsOrganizer(isOrganizer);
+          console.log('isOrganizer', isOrganizer);
+        }
+      })
+      .catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
