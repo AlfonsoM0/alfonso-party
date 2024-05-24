fa5447c5455ff529d60d169bf6fe3162a4a24193
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-24 09:53:14 -0300

    feat: isOrganizer

--- src/hooks/use-vip-url-state.tsx
+++ src/hooks/use-vip-url-state.tsx
@@ -5,14 +5,18 @@ import { persist } from 'zustand/middleware';
 
 interface vipUrlStateProps {
   vipUrl: string;
+  isOrganizer: boolean;
   setVipUrl: (vipUrl: string) => void;
+  setIsOrganizer: (isOrganizer: boolean) => void;
 }
 
 export const useVipUrlState = create<vipUrlStateProps>()(
   persist(
     (set) => ({
       vipUrl: '',
+      isOrganizer: false,
       setVipUrl: (vipUrl) => set({ vipUrl }),
+      setIsOrganizer: (isOrganizer) => set({ isOrganizer }),
     }),
     {
       name: 'vip-url-storage',
