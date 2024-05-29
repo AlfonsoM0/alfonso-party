427e326fcd6f42775abef0234eda6aece30ef300
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-05-29 15:17:42 -0300

    Show Time

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -15,14 +15,16 @@ export default async function Page() {
 
   return (
     <>
-      <BgVideo vipsUrls={vipsUrls} videosUrls={['/MMEventos.mp4']} isWhiteBg />
-      <main className="flex h-[87vh] max-w-xs m-auto flex-col items-center justify-center gap-12">
+      <BgVideo vipsUrls={vipsUrls} videosUrls={['/MMEventos.mp4']} />
+      <main className="flex h-[87vh] max-w-xs m-auto flex-col gap-12">
         <GuestNav />
 
-        <div>
-          <h1>Próximamente</h1>
-          <p>¡No seas ansiosa!</p>
-          <p>Compartiré la ubicación, o lo harán los organizadores de cada grupo.</p>
+        <div className="mt-24 [&_p]:mb-2">
+          {/* <h1>Próximamente</h1> */}
+          <h2 className="mb-3">¡No seas ansiosa!</h2>
+          <p>La ubicación será compartida por los organizadores de cada grupo.</p>
+          <p>Si NO estás en uno de esos grupos, puedes pedirme la ubicación por WhatsApp.</p>
+          <p className="mt-5">¡Gracias!</p>
         </div>
       </main>
       <Footer />
